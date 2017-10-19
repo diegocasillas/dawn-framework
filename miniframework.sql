d9cc4396dcffdbec853ad29661dfb2db2d4ae51a
Diego Casillas <riskydiego@hotmail.com>
2017-10-19 20:00:57 +0200

    Add sql

---
+++ miniframework.sql
@@ -0,0 +1,58 @@
+-- --------------------------------------------------------
+-- Host:                         127.0.0.1
+-- Server version:               10.1.26-MariaDB - mariadb.org binary distribution
+-- Server OS:                    Win32
+-- HeidiSQL Version:             9.4.0.5125
+-- --------------------------------------------------------
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET NAMES utf8 */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+
+
+-- Dumping database structure for miniframework
+CREATE DATABASE IF NOT EXISTS `miniframework` /*!40100 DEFAULT CHARACTER SET utf8 */;
+USE `miniframework`;
+
+-- Dumping structure for table miniframework.comments
+CREATE TABLE IF NOT EXISTS `comments` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `post_id` int(11) NOT NULL DEFAULT '0',
+  `author` int(11) NOT NULL,
+  `body` text NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `FK_comments_posts` (`post_id`),
+  CONSTRAINT `FK_comments_posts` FOREIGN KEY (`post_id`) REFERENCES `posts` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=31 DEFAULT CHARSET=utf8;
+
+-- Dumping data for table miniframework.comments: ~7 rows (approximately)
+/*!40000 ALTER TABLE `comments` DISABLE KEYS */;
+INSERT INTO `comments` (`id`, `post_id`, `author`, `body`) VALUES
+	(27, 29, 0, 'First comment'),
+	(28, 29, 0, 'Second comment'),
+	(29, 31, 0, 'First comment'),
+	(30, 30, 0, 'And the first comment xD');
+/*!40000 ALTER TABLE `comments` ENABLE KEYS */;
+
+-- Dumping structure for table miniframework.posts
+CREATE TABLE IF NOT EXISTS `posts` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `author` varchar(50) NOT NULL DEFAULT '0',
+  `title` varchar(50) NOT NULL DEFAULT '0',
+  `body` text NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=32 DEFAULT CHARSET=utf8;
+
+-- Dumping data for table miniframework.posts: ~28 rows (approximately)
+/*!40000 ALTER TABLE `posts` DISABLE KEYS */;
+INSERT INTO `posts` (`id`, `author`, `title`, `body`) VALUES
+	(29, 'Anon', 'First post', 'First comment'),
+	(30, 'Anon', 'Second post', 'Lol, the second post'),
+	(31, 'Anon', 'Third post', 'this is the third post');
+/*!40000 ALTER TABLE `posts` ENABLE KEYS */;
+
+/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
+/*!40014 SET FOREIGN_KEY_CHECKS=IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
