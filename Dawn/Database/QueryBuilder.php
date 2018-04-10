2d20141aade256136fbcda0b327622cb303d780d
Diego Casillas <diegocasillasdev@gmail.com>
2018-04-10 14:41:07 +0200

    Add insert feature for QueryBuilder

--- Dawn/Database/QueryBuilder.php
+++ Dawn/Database/QueryBuilder.php
@@ -4,11 +4,16 @@ namespace Dawn\Database;
 
 class QueryBuilder
 {
-    protected $query;
+    protected $app;
+    protected $connection;
     protected $model;
+    protected $query;
+    protected $statement;
 
-    public function __construct($model = null)
+    public function __construct($app = null, $connection = null, $model = null)
     {
+        $this->app = $app;
+        $this->connection = $connection;
         $this->model = $model;
     }
 
@@ -55,8 +60,62 @@ class QueryBuilder
         return "$column$operator$value";
     }
 
+    public function insert($table, array $columns = [], array $values)
+    {
+        if (!empty($columns)) {
+            $columnsString = "(" . implode(', ', $columns) . ")";
+        }
+
+        $this->query .= "INSERT INTO " . $table . $columnsString . " VALUES(" . $this->makeValuesString($table, $columns, $values) . ")";
+        return $this;
+    }
+
+    public function stringOrNumber($table, $column)
+    {
+        $sql = "SELECT " . $column . " FROM " . $table;
+        $statement = $this->connection->query($sql);
+        // die(var_dump($sql));
+        $meta = $statement->getColumnMeta(0);
+
+        return $meta['native_type'];
+    }
+
+    public function makeValuesString($table, $columns, $values)
+    {
+        $valuesString = "";
+
+        foreach ($values as $index => $value) {
+            echo $value;
+            var_dump($this->stringOrNumber($table, $columns[$index]));
+            if ($this->stringOrNumber($table, $columns[$index]) === "number") {
+                $valuesString .= "$value";
+            } else {
+                $valuesString .= "'$value'";
+            }
+        }
+        echo $valuesString;
+        die();
+        return $valuesString;
+    }
+
+    public function exec($query = null)
+    {
+        if ($query !== null) {
+            $this->query = $query;
+        }
+
+        $this->statement = $this->connection->prepare($this->query);
+        $this->query = null;
+        return $this->statement->execute();
+    }
+
     public function getQuery()
     {
         return $this->query;
+    }
+
+    public function getStatement()
+    {
+        return $this->statement;
     }
 }
