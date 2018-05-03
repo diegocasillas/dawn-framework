5fb55c671177f3b4f8559005d878cce0f0cf40f1
Diego Casillas <diegocasillasdev@gmail.com>
2018-05-03 12:28:24 +0200

    Document ServiceProvider

--- Dawn/App/ServiceProvider.php
+++ Dawn/App/ServiceProvider.php
@@ -2,11 +2,24 @@
 
 namespace Dawn\App;
 
+/**
+ * Class to be extended by different service providers.
+ */
 abstract class ServiceProvider implements ServiceProviderInterface
 {
+    /**
+     * The application container instance.
+     *
+     * @var App
+     */
     protected $app;
 
-    public function __construct($app)
+    /**
+     * Create a new Service Provider instance.
+     *
+     * @param App $app
+     */
+    public function __construct(App $app)
     {
         $this->app = $app;
     }
--- Dawn/App/ServiceProviderInterface.php
+++ Dawn/App/ServiceProviderInterface.php
@@ -2,9 +2,22 @@
 
 namespace Dawn\App;
 
+/**
+ * Interface to be implemented by service providers.
+ */
 interface ServiceProviderInterface
 {
+    /**
+     * Bind a service to the application container.
+     *
+     * @return void
+     */
     public function register();
 
+    /**
+     * Bootstrap the service.
+     *
+     * @return void
+     */
     public function boot();
 }
