00c88e51afed9954c43d14339533c1fd6a6c351b
Diego Casillas <diegocasillasdev@gmail.com>
2018-05-14 14:10:21 +0200

    Catch .env error

--- Dawn/App/bootstrap.php
+++ Dawn/App/bootstrap.php
@@ -2,8 +2,15 @@
 
 require 'vendor/autoload.php';
 
-$dotenv = new Dotenv\Dotenv(dirname(__DIR__, 2));
-$dotenv->load();
+try {
+  $dotenv = new Dotenv\Dotenv(dirname(__DIR__, 2));
+  $dotenv->load();
+} catch (Exception $e) {
+  echo "There was an error reading the /.env file. Copy it from /example.env.";
+  echo "<hr>";
+  echo $e->getMessage();
+  die();
+}
 
 define('CONFIG', require 'config.php');
 
