b0cabe448d2a6499464ff7586000f081ffa8b2fc
Diego Casillas <diegocasillasdev@gmail.com>
2018-05-15 12:52:57 +0200

    Document RoutingServiceProvider.php

--- Dawn/Routing/RoutingServiceProvider.php
+++ Dawn/Routing/RoutingServiceProvider.php
@@ -4,25 +4,48 @@ namespace Dawn\Routing;
 
 use Dawn\App\ServiceProvider;
 
+/**
+ * Registers and boots routing services.
+ */
 class RoutingServiceProvider extends ServiceProvider
 {
+    /**
+     * Register the services.
+     *
+     * @return void
+     */
     public function register()
     {
         $this->registerControllerDispatcher();
         $this->registerRouter();
     }
 
+    /**
+     * Bootstrap the services.
+     *
+     * @return void
+     */
     public function boot()
     {
 
     }
 
+    /**
+     * Create a new Controller Dispatcher instance and bind it to the application container.
+     *
+     * @return void
+     */
     private function registerControllerDispatcher()
     {
         $controllerDispatcher = new ControllerDispatcher($this->app);
         $this->app->bind('controller dispatcher', $controllerDispatcher);
     }
 
+    /**
+     * Create a new Router instance and bind it to the application container.
+     *
+     * @return void
+     */
     private function registerRouter()
     {
         $router = new Router($this->app, $this->app->getConfig()['routes']);
