355f2bc73c691c9da14c66adae0de95b5f39003d
Diego Casillas <diegocasillasdev@gmail.com>
2018-05-15 10:48:06 +0200

    Document ControllerDispatcher.php

--- Dawn/Routing/ControllerDispatcher.php
+++ Dawn/Routing/ControllerDispatcher.php
@@ -86,4 +86,109 @@ class ControllerDispatcher
     {
         return $this->dispatchedController->callAction($this->action, $this->parameters, $this->options);
     }
+
+    /**
+     * Get the app instance.
+     *
+     * @return Dawn\App\App
+     */
+    public function getApp()
+    {
+        return $this->app;
+    }
+
+    /**
+     * Set the app instance.
+     *
+     * @param Dawn\App\App $app
+     * @return void
+     */
+    public function setApp($app)
+    {
+        $this->app = $app;
+    }
+
+    /**
+     * Get the dispatched controller instance.
+     *
+     * @return Dawn\Routing\Controller
+     */
+    public function getDispatchedController()
+    {
+        return $this->dispatchedController;
+    }
+
+    /**
+     * Set the dispatched controller instance
+     *
+     * @param Dawn\Routing\Controller $dispatchedController
+     * @return void
+     */
+    public function setDispatchedController($dispatchedController)
+    {
+        $this->dispatchedController = $dispatchedController;
+    }
+
+    /**
+     * Get the action name.
+     *
+     * @return string
+     */
+    public function getAction()
+    {
+        return $this->action;
+    }
+
+    /**
+     * Set the action name.
+     *
+     * @param string $action
+     * @return void
+     */
+    public function setAction($action)
+    {
+        $this->action = $action;
+    }
+
+    /**
+     * Get the parameters array-
+     *
+     * @return array
+     */
+    public function getParameters()
+    {
+        return $this->parameters;
+    }
+
+    /**
+     * Set the parameters array.
+     *
+     * @param array $parameters
+     * @return void
+     */
+    public function setParameters($parameters)
+    {
+        $this->parameters = $parameters;
+    }
+
+    /**
+     * Get the options array.
+     *
+     * @return array
+     */
+    public function getOptions()
+    {
+        return $this->options;
+    }
+
+    /**
+     * Set the options array.
+     *
+     * @param array $options
+     * @return void
+     */
+    public function setOptions($options)
+    {
+        $this->options = $options;
+    }
 }
