5d1ab03e27d169ab26412edfbfa4010b7a9cc656
Diego Casillas <diegocasillasdev@gmail.com>
2018-05-15 13:17:00 +0200

    Document SessionServiceProvider.php

--- Dawn/Session/SessionServiceProvider.php
+++ Dawn/Session/SessionServiceProvider.php
@@ -4,18 +4,36 @@ namespace Dawn\Session;
 
 use Dawn\App\ServiceProvider;
 
+/**
+ * Registers and boots session services.
+ */
 class SessionServiceProvider extends ServiceProvider
 {
+    /**
+     * Register the services.
+     *
+     * @return void
+     */
     public function register()
     {
         $this->registerSession();
     }
 
+    /**
+     * Bootstrap the services.
+     *
+     * @return void
+     */
     public function boot()
     {
         $session = $this->app->get('session');
     }
 
+    /**
+     * Create a new Session instance and bind it to the application container.
+     *
+     * @return void
+     */
     private function registerSession()
     {
         $session = new Session($this->app, $this->app->getConfig()['session']);
