64a567dfe632348517b4cc782b8908d6a5833f00
Diego Casillas <diegocasillasdev@gmail.com>
2018-05-16 12:12:58 +0200

    Create Model.php

---
+++ app/models/Model.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Models;
+
+use Dawn\Database\Model as DawnModel;
+
+// You can extend the base model here. Your models should extend this class.
+
+class Model extends DawnModel
+{
+  public function __construct()
+  {
+    parent::__construct();
+  }
+}
--- app/routes/web.php
+++ app/routes/web.php
@@ -1,33 +1,33 @@
 <?php
 
-$this->get('miniframework/apitest', 'PostController', 'api');
+$this->get('dawn/apitest', 'PostController', 'api');
 
 // Login, logout and register
-$this->get('miniframework/login', 'Auth\LoginController', 'showLoginForm')->auth('guest');
-$this->post('miniframework/login', 'Auth\LoginController', 'login')->auth('guest');
-$this->get('miniframework/logout', 'Auth\LoginController', 'logout')->auth('authenticated');
-$this->get('miniframework/register', 'Auth\RegisterController', 'showRegistrationForm')->auth('guest');
-$this->post('miniframework/register', 'Auth\RegisterController', 'register')->auth('guest');
+$this->get('dawn/login', 'Auth\LoginController', 'showLoginForm')->auth('guest');
+$this->post('dawn/login', 'Auth\LoginController', 'login')->auth('guest');
+$this->get('dawn/logout', 'Auth\LoginController', 'logout')->auth('authenticated');
+$this->get('dawn/register', 'Auth\RegisterController', 'showRegistrationForm')->auth('guest');
+$this->post('dawn/register', 'Auth\RegisterController', 'register')->auth('guest');
 
 // Users
-$this->get('miniframework/users/{id}', 'UserController', 'show')->auth('authenticated');
-$this->get('miniframework/users/{id}/edit', 'UserController', 'edit')->auth('owner');
-$this->post('miniframework/users/{id}/edit', 'UserController', 'edit')->auth('owner');
+$this->get('dawn/users/{id}', 'UserController', 'show')->auth('authenticated');
+$this->get('dawn/users/{id}/edit', 'UserController', 'edit')->auth('owner');
+$this->post('dawn/users/{id}/edit', 'UserController', 'edit')->auth('owner');
 
 // Posts
-$this->get('miniframework', 'PostController', 'index')->auth('authenticated');
-$this->get('miniframework/posts', 'PostController', 'index')->auth('authenticated');
-$this->get('miniframework/api', 'PostController', 'apiIndex')->auth('authenticated');
-$this->get('miniframework/posts/create', 'PostController', 'create')->auth('authenticated');
-$this->post('miniframework/posts/create', 'PostController', 'store')->auth('authenticated');
-$this->get('miniframework/posts/{id}', 'PostController', 'show')->auth('authenticated');
-$this->get('miniframework/posts/{id}/edit', 'PostController', 'edit')->auth('owner');
-$this->post('miniframework/posts/{id}/edit', 'PostController', 'update')->auth('owner');
+$this->get('dawn', 'PostController', 'index')->auth('authenticated');
+$this->get('dawn/posts', 'PostController', 'index')->auth('authenticated');
+$this->get('dawn/api', 'PostController', 'apiIndex')->auth('authenticated');
+$this->get('dawn/posts/create', 'PostController', 'create')->auth('authenticated');
+$this->post('dawn/posts/create', 'PostController', 'store')->auth('authenticated');
+$this->get('dawn/posts/{id}', 'PostController', 'show')->auth('authenticated');
+$this->get('dawn/posts/{id}/edit', 'PostController', 'edit')->auth('owner');
+$this->post('dawn/posts/{id}/edit', 'PostController', 'update')->auth('owner');
 
 // Comments
-$this->post('miniframework/posts/{id}', 'CommentController', 'store')->auth('authenticated');
-$this->post('miniframework/posts/{id}/vote', 'PostController', 'vote')->auth('authenticated');
+$this->post('dawn/posts/{id}', 'CommentController', 'store')->auth('authenticated');
+$this->post('dawn/posts/{id}/vote', 'PostController', 'vote')->auth('authenticated');
 
 // Errors
-$this->get('miniframework/404', 'ErrorController', 'notFound');
-$this->get('miniframework/401', 'ErrorController', 'unauthorized');
+$this->get('dawn/404', 'ErrorController', 'notFound');
+$this->get('dawn/401', 'ErrorController', 'unauthorized');
